42d9e297e079f838e385609c8cb3648d932179b4
stefanpython <stefanhtml@yahoo.com>
2023-11-23 16:26:23 +0200

    Add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,75 @@
-import "./Navbar.css"
-import { Link } from "react-router-dom"
+import "./Navbar.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
 
-    return (
-        <div className="navbar">
-        <div className="navbar-left">
-          <img className="logo" src="./security.png" alt="logo" />
-          <span className="navbar-title">
-           Wizdom Wave
-          </span>
-        </div>
-  
-        <div className="navbar-right">
-         
-          <ul className="desktop-links">
-           
-          </ul>
-  
-        
-        </div>
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img className="logo" src="" alt="logo" />
+        <span className="navbar-title">
+          <span className="color-title">Wizdom</span> Wave
+        </span>
       </div>
-    )
+
+      <div className="navbar-right">
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776; {/* Unicode for hamburger icon */}
+        </div>
+
+        <ul className="desktop-links">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/services"}>Services</Link>
+          </li>
+          <li>
+            <Link to={"/fees"}>Fees</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li>
+            <Link to={"/testimonials"}>Testimonials</Link>
+          </li>
+        </ul>
+
+        <ul
+          className={`navbar-links ${showMenu ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/services"}>Services</Link>
+          </li>
+          <li>
+            <Link to={"/fees"}>Fees</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li>
+            <Link to={"/testimonials"}>Testimonials</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
