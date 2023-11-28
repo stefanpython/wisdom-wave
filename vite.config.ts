5069b82f5f0725490c4f664570159fc46ee93147
stefanpython <stefanhtml@yahoo.com>
2023-11-28 17:46:23 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://stefanpython.github.io/wisdom-wave",
   plugins: [react()],
 })
